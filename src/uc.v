6939aade21805350ed5294003bf8297d7548fdbb
leosalgado <leonardosalgado@protonmail.com>
2025-06-09 01:11:15 -0300

    Adjust uc

--- src/uc.v
+++ src/uc.v
@@ -1,39 +1,78 @@
-module uc (opcode, alu_op, mem_read, mem_write);
+module uc (opcode, alu_op, mem_read, mem_write, reg_write, mem_to_reg, alu_src, branch);
 
-    input [3:0] opcode;
+    input [6:0] opcode;
     output reg [1:0] alu_op;
     output reg mem_read;
     output reg mem_write;
+    output reg reg_write;
+    output reg mem_to_reg;
+    output reg alu_src;
+    output reg branch;
 
     always @(*) begin
+        
+        alu_op = 2'bxx;
+        mem_read = 1'b0;
+        mem_write = 1'b0;
+        reg_write = 1'b0;
+        mem_to_reg = 1'b0;
+        alu_src = 1'b0;
+        branch = 1'b0;
 
         case (opcode)
-            4'b0000: begin // ADD
-                alu_op = 2'b00;
-                mem_read = 0;
-                mem_write = 0;
+
+            7'b0000011: begin
+                alu_op = 2'b00; 
+                mem_read = 1'b1;   
+                mem_write = 1'b0;   
+                reg_write = 1'b1;   
+                mem_to_reg = 1'b1;   
+                alu_src = 1'b1;   
+                branch = 1'b0;  
             end
-            4'b0001: begin // SUB
-                alu_op = 2'b01;
-                mem_read = 0;
-                mem_write = 0;
+
+            7'b0100011: begin 
+                alu_op = 2'b00; 
+                mem_read = 1'b0;   
+                mem_write = 1'b1;   
+                reg_write = 1'b0;   
+                mem_to_reg = 1'b0;  
+                alu_src = 1'b1;   
+                branch = 1'b0;   
             end
-            4'b0010: begin // LOAD
-                alu_op = 2'b10;
-                mem_read = 1;
-                mem_write = 0;
+
+            7'b1100011: begin 
+                alu_op     = 2'b01; 
+                mem_read   = 1'b0;   
+                mem_write  = 1'b0;  
+                reg_write  = 1'b0;   
+                mem_to_reg = 1'b0;   
+                alu_src    = 1'b0;   
+                branch     = 1'b1;   
             end
-            4'b0011: begin // STORE
-                alu_op = 2'b10;
-                mem_read = 0;
-                mem_write = 1;
+
+            7'b0110011: begin 
+                alu_op     = 2'b10;  
+                mem_read   = 1'b0;   
+                mem_write  = 1'b0;   
+                reg_write  = 1'b1;   
+                mem_to_reg = 1'b0;  
+                alu_src    = 1'b0;   
+                branch     = 1'b0;   
             end
+
             default: begin
-                alu_op = 2'bxx;
-                mem_read = 0;
-                mem_write = 0;
+                alu_op     = 2'b00; 
+                mem_read   = 1'b0;
+                mem_write  = 1'b0;
+                reg_write  = 1'b0;
+                mem_to_reg = 1'b0;
+                alu_src    = 1'b0;
+                branch     = 1'b0;
             end
+
         endcase
 
     end
+
 endmodule
--- tb/uc_tb.v
+++ tb/uc_tb.v
@@ -3,27 +3,42 @@
 
 module uc_tb;
 
-    reg [3:0] opcode;
+    reg [6:0] opcode;
     wire [1:0] alu_op;
     wire mem_read;
     wire mem_write;
+    wire reg_write;
+    wire mem_to_reg;
+    wire alu_src;
+    wire branch;
 
-    uc uut(opcode, alu_op, mem_read, mem_write);
+    uc uut (opcode, alu_op, mem_read, mem_write, reg_write, mem_to_reg, alu_src, branch);
 
     initial begin
-
+        
         $dumpfile("waveforms/uc_tb.vcd");
         $dumpvars(0, uc_tb);
 
-        opcode = 4'b0000; #20 // ADD
-        opcode = 4'b0001; #20 // SUB
-        opcode = 4'b0010; #20 // LOAD
-        opcode = 4'b0011; #20 // STORE
-        opcode = 4'b0100; #20 // DEFAULT (invalid opcode)
+        opcode = 7'b0000000;
+        #10;
+
+        opcode = 7'b0000011;
+        #10;
+
+        opcode = 7'b0100011;
+        #10;
+
+        opcode = 7'b1100011;
+        #10;
+
+        opcode = 7'b0110011;
+        #10;
+
+        opcode = 7'b1111111;
+        #10;
 
         $display("Teste Completo");
 
     end
 
 endmodule
-
